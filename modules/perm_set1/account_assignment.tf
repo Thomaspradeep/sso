e5bcc4313398a9c2d781e4954f7a393218706e89
Thomas pradeep <60232180+Thomaspradeep@users.noreply.github.com>
2024-03-30 22:26:59 +0530

    Create account_assignment.tf

---
+++ modules/perm_set1/account_assignment.tf
@@ -0,0 +1,8 @@
+resource "aws_ssoadmin_account_assignment" "account_assignment" {
+  instance_arn       = aws_ssoadmin_permission_set.client_perm_set.instance_arn
+  permission_set_arn = aws_ssoadmin_permission_set.client_perm_set.arn
+  principal_type     = "USER"
+  principal_id       = "c4689408-80b1-7075-4384-06471b80a0fd"
+  target_type        = "AWS_ACCOUNT"
+  target_id          = var.account_id
+}
