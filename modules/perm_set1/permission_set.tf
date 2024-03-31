181949474d2fc28f4092a0f66e9ebb3af2f66db1
Thomas pradeep <60232180+Thomaspradeep@users.noreply.github.com>
2024-03-31 21:30:24 +0530

    Update permission_set.tf

--- modules/perm_set1/permission_set.tf
+++ modules/perm_set1/permission_set.tf
@@ -1,8 +1,15 @@
-data "aws_ssoadmin_permission_set" "permission_set" {
-  instance_arn = var.instance_arn
-  name         = var.client_name
-}
+# data "aws_ssoadmin_permission_set" "permission_set" {
+#   instance_arn = var.instance_arn
+#   name         = var.client_name
+# }
 
-output "arn" {
-  value = data.aws_ssoadmin_permission_set.permission_set.arn
+# output "arn" {
+#   value = data.aws_ssoadmin_permission_set.permission_set.arn
+# }
+
+resource "aws_ssoadmin_permission_set" "client_perm_set" {
+  name        = var.client_name
+  description = "Your Permission Set Description"
+  instance_arn = var.instance_arn
+  session_duration = "PT2H"
 }
