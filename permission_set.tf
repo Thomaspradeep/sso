5fde5c109f983acdbc46ecfeb522a3bfee34a711
Thomas pradeep <60232180+Thomaspradeep@users.noreply.github.com>
2024-04-12 11:24:18 +0530

    Update permission_set.tf

--- permission_set.tf
+++ permission_set.tf
@@ -5,10 +5,3 @@ client_name = each.key
 instance_arn = var.instance_arn
 account_id = var.account_id
 }
-module d3_client_perm_set {
-source = "./modules/perm_set1"
-for_each = var.client_list
-client_name = each.key
-instance_arn = var.instance_arn
-account_id = var.account_id
-}
