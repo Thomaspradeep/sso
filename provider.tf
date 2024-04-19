a5bca444fcd2d274b720b050462d2ed5cb73072d
pradeep <thomaspradeep06@gmail.com>
2024-04-19 21:44:03 +0530

    bucket name updated

--- provider.tf
+++ provider.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "matthews-bucket-91423"
+    bucket = "matthew-bucket-91423"
     key    = "sso/terraform.tfstate"
     region = "ap-south-1"
   }
